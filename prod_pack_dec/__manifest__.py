6bc8b6621be40070fad8d3f3c26b8ca7cb2d37b0
Jeff Rund <jeff.rund@taluserp.com>
2020-06-26 10:36:52 -0400

    PO Mods-update depends to add purchase

--- prod_pack_dec/__manifest__.py
+++ prod_pack_dec/__manifest__.py
@@ -19,7 +19,7 @@
     'version': '0.4',
 
     # any module necessary for this one to work correctly
-    'depends': ['base','stock'],
+    'depends': ['base','purchase'],
 
     # always loaded
     'data': [
